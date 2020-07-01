4be81cb30134b5438cfb451d3c7cf6dc40a24448
dingxingkai <17600299358@163.com>
2020-07-01 22:56:26 +0800

    Java基础Demo、SpringBoot集成各中间件模块 2020年7月1日22:56:16 dxk

---
+++ SpringBoot-Testing/src/main/java/com/dxk/springboot/testing/basic/BasicTest.java
@@ -0,0 +1,18 @@
+package com.dxk.springboot.testing.basic;
+
+/**
+ * Created with IDEA.2020
+ * User: dxk
+ * Date: 2020/7/1
+ * Time: 22:03
+ * Description: 基础知识点
+ */
+public class BasicTest {
+    public static void main(String[] args) {
+        String s1 = "123";
+        String s2 = "123";
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+
+    }
+}
