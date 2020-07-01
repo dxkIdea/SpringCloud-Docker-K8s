1e7044a46ef4927bcd4dfa8b1d4a79cdd4716894
dingxingkai <17600299358@163.com>
2020-07-01 21:24:21 +0800

    kafka定时任务推数 2020年7月1日21:24:12 dxk

--- SpringCloud-Nacos-Provider/src/main/java/com/dxk/spring/nacos/provider/scheduling/KafkaSchedulingTestService.java
+++ SpringCloud-Nacos-Provider/src/main/java/com/dxk/spring/nacos/provider/scheduling/KafkaSchedulingTestService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 /**
  * Created with IDEA.2020
@@ -26,9 +27,17 @@ public class KafkaSchedulingTestService {
     @Scheduled(cron = "*/10 * * * * ?")
     public void sendMessage () {
         try{
-            String msg = "当前时间戳为---" + System.currentTimeMillis();
-            kafkaTemplate.send("testTopic",msg);
-            System.out.println("发送消息：" + msg);
+            kafkaTemplate.send("testTopic", UUID.randomUUID().toString(),"时间戳- " + System.currentTimeMillis()).addCallback( success -> {
+                // 消息发送到的topic
+                String topic = success.getRecordMetadata().topic();
+                // 消息发送到的分区
+                int partition = success.getRecordMetadata().partition();
+                // 消息在分区内的offset
+                long offset = success.getRecordMetadata().offset();
+                System.out.println("发送消息成功:" + topic + "-" + partition + "-" + offset);
+            },failure -> {
+                System.out.println("发送消息失败:" + failure.getMessage());
+            });
         }catch (Exception ex) {
             ex.printStackTrace();
         }
